c67d0e3c2f4d3ea66dc133deaf921dcbe34ca632
JackDownload <kismocsy@gmail.com>
2020-11-10 13:00:02 +0100

    Update comfortable.js

--- src/comfortable.js
+++ src/comfortable.js
@@ -1,3 +1,3 @@
 import Comfortable from 'comfortable-javascript';
 
-export const comfortable = Comfortable.api('vassCms', '2724tVQfWW2jto1ER5Mw6bL3QSJYGKNg');
+export const comfortable = Comfortable.api('vassCms', 's6XvQFgnqNG5sSsOI5uRxEWSU3ZxsfMj');
